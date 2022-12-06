95d6aab043f4a6a04766940f35cadbc0a4f6968c
Arno <31947147+Alwaysproblem@users.noreply.github.com>
2022-12-06 10:49:58 +0800

    Mlir tutorial (#2)
    * add all toy chapter examples
    
    * add some example
    
    * example for ch3
    
    * update
    
    * add toy example

--- mlir/example/build.sh
+++ mlir/example/build.sh
@@ -5,14 +5,19 @@ _target=${1:-'all'}
 rm -rf build
 mkdir build
 
+_workspaceFolder=$(pwd)
+
 cd build
 
-cmake .. \
+cmake ..   -G Ninja \
   -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
   -DCMAKE_BUILD_TYPE:STRING=Debug \
   -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc \
   -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ \
-  -G Ninja
+  -DMLIR_DIR=${_workspaceFolder}/third_party/lib/cmake/mlir \
+  -DLLVM_DIR=${_workspaceFolder}/third_party/lib/cmake/llvm \
+  -DCMAKE_MODULE_PATH=${_workspaceFolder}/third_party/lib/cmake/mlir;${_workspaceFolder}/third_party/lib/cmake/llvm \
+  -DMLIR_TABLEGEN_EXE=${_workspaceFolder}/third_party/bin/mlir-tblgen
 
 # ninja
 cmake \
