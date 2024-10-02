e31965da9a599bab51e6b729fe87149d6f72819c
Alwaysproblem <reganyang0415@gmail.com>
2024-10-02 12:58:51 +0000

    Added Action Example

--- mlir/mhlo-pass-example/Pow2/mlir/Pow2.cpp
+++ mlir/mhlo-pass-example/Pow2/mlir/Pow2.cpp
@@ -9,6 +9,7 @@
 #include <llvm/Support/Format.h>
 #include <llvm/Support/raw_ostream.h>
 #include <mlir/Analysis/CallGraph.h>
+#include <mlir/IR/Action.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/PatternMatch.h>
@@ -18,12 +19,43 @@
 #include <mlir/Transforms/Passes.h>
 
 #include "passes/Pow2.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
 
 #include <memory>
 #include <numeric>
 
 using namespace mlir;
+
+namespace {
+// We add MLIR actions here as an example.
+/// A custom Action can be defined minimally by deriving from
+/// `tracing::ActionImpl`. The action is same as the pass declaration with tddr
+/// rules. only for `xxx-opt` binary. and run with `--log-actions-to=-` to dump
+/// the actions.
+class EchoAction : public tracing::ActionImpl<EchoAction> {
+public:
+  using Base = tracing::ActionImpl<EchoAction>;
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(EchoAction)
+
+  /// Actions are initialized with an array of IRUnit (that is either Operation,
+  /// Block, or Region) that provide context for the IR affected by a
+  /// transformation.
+  EchoAction(ArrayRef<IRUnit> irUnits, int iteration)
+      : Base(irUnits), iteration(iteration) {}
+  /// This tag should uniquely identify this action, it can be matched for
+  /// filtering during processing.
+  static constexpr StringLiteral tag = "echo-action";
+  static constexpr StringLiteral desc = "Just echo the iteration";
+
+  void print(raw_ostream &os) const override {
+    os << "EchoAction: " << iteration << "\n";
+  }
+
+private:
+  int iteration;
+};
+} // namespace
 
 namespace {
 bool ValueEql2(Value operand) {
@@ -103,13 +135,24 @@ struct SubstitutePow2PdllGenPass
     : impl::Pow2PassBase<SubstitutePow2PdllGenPass> {
   void runOnOperation() final {
     auto op = getOperation();
-    RewritePatternSet patterns(&getContext());
+    MLIRContext *context = &getContext();
+    RewritePatternSet patterns(&*context);
     // --- insert the native constraints ---
     registerNativeConstraints(patterns);
     // --- insert the native constraints ---
-    patterns.add<Pow2PdllOptPattern>(&getContext());
-    if (failed(applyPatternsAndFoldGreedily(op, std::move(patterns))))
-      signalPassFailure();
+    patterns.add<Pow2PdllOptPattern>(&*context);
+    // Here, we wrap the applyPatternsAndFoldGreedily in a lambda function and
+    // pass it to the MLIR Action.
+    Operation *opp = getOperation();
+    ArrayRef<IRUnit> irUnits{opp};
+    context->executeAction<EchoAction>(
+        [&]() {
+          // Here is the pass body.
+          if (failed(applyPatternsAndFoldGreedily(op, std::move(patterns))))
+            signalPassFailure();
+        },
+        /*irUnits=*/irUnits, /*iteration=*/10);
+        // Above, we pass the irUnits and iteration to the EchoAction.
   };
 };
 } // namespace
@@ -128,7 +171,7 @@ std::unique_ptr<mlir::Pass> mhlo::createSubstitutePow2Pass() {
 struct StaticOpCounterAnalysis {
   llvm::StringMap<int> opCount;
   // Compute this analysis with the provided operation.
-  StaticOpCounterAnalysis(Operation *op) : opCount({}){};
+  StaticOpCounterAnalysis(Operation *op) : opCount({}) {};
 
   void add(Operation *op) {
     auto opName = op->getName().getStringRef();
